733de0d881e4b3216243d68e25f3ca3d3cae7c4d
Alexander Psiuk <alexander.psiuk@googlemail.com>
2016-01-01 21:01:39 +0100

    refactored gallery creation

--- expose.py
+++ expose.py
@@ -1,210 +1,268 @@
 from collections import OrderedDict
-from PIL import Image
-from time import strptime, strftime
+import jinja2
 import json
 import markdown
+import shutil
 import os
-import pprint
 import re
+from PIL import Image
 
-inputFolder = "input"
+
+# --- global variables --------------------------------------------------------
 imageTypes = ["jpg", "png", "gif"]
+videoTypes = ["mp4"]
 textTypes = ["txt", "md"]
-md = markdown.Markdown(extensions = ['markdown.extensions.meta'])
-imageIndex = 0
+markdownParser = markdown.Markdown(extensions = ["markdown.extensions.meta"])
 
-def natural_key(string_):
-    """See http://www.codinghorror.com/blog/archives/001018.html"""
-    return [int(s) if s.isdigit() else s for s in re.split(r'(\d+)', string_["sort"][0])]
-
-def natural_key(string_):
-    """See http://www.codinghorror.com/blog/archives/001018.html"""
-    return [int(s) if s.isdigit() else s for s in re.split(r'(\d+)', string_)]
-
-
-# --- helpers -----------------------------------------------------------------
-def resizeImage(sourceImage, destinationImage, width):
-	print("Resizing:", sourceImage)
-	if not os.path.exists(os.path.dirname(destinationImage)):
-		os.makedirs(os.path.dirname(destinationImage))
-	img = Image.open(sourceImage)
-	wpercent = (width/float(img.size[0]))
-	hsize = int((float(img.size[1])*float(wpercent)))
-	img = img.resize((width,hsize), Image.ANTIALIAS)
-	#faster: img = img.resize((width,hsize))
-	img.save(destinationImage)
-	return destinationImage.replace("output/", "")
-
-
-def getSnippet(buffer):
-	global imageIndex
-	html = ""
-
-	date = buffer.get("meta", {}).get("date", ["none"])[0]
-	if date is not "0":
-		html += "<a name='" + date + "'></a>"
-
-	# Text only
-	if "text" in buffer and "image" not in buffer:
-		template = getTemplate("content-text.html")		
-		content = buffer["text"]
-		html += template.replace("{{content}}", content)
-		return html
-
-	# Image only
-	if "image" in buffer and "text" not in buffer:
-		imageIndex += 1
-		template = getTemplate("content-image.html")		
-		outputImage = buffer["image"].replace("input", "output")
-		content = resizeImage(buffer["image"], outputImage, 1000)
-		contentbig = "../" + buffer["image"]
-		template = template.replace("{{imageIndex}}", str(imageIndex))
-		template = template.replace("{{content}}", content)
-		template = template.replace("{{contentbig}}", contentbig)
-		html += template
-		return html
-
-	# Text and Image
-	if "image" in buffer and "text" in buffer:
-		imageIndex += 1
-		template = getTemplate("content-textimage.html")		
-		outputImage = buffer["image"].replace("input", "output")
-		content = resizeImage(buffer["image"], outputImage, 1000)
-		contentbig = "../" + buffer["image"]
-		type = buffer.get("meta", {}).get("type", ["default"])[0]
-		style = getCSS(buffer)
-		caption = buffer["text"]
-
-		template = template.replace("{{content}}", content)
-		template = template.replace("{{contentbig}}", contentbig)
-		template = template.replace("{{imageIndex}}", str(imageIndex))
-		template = template.replace("{{type}}", type)
-		template = template.replace("{{style}}", style)
-		template = template.replace("{{caption}}", caption)
-		html += template
-		return html
-
-
-def getCSS(data):
-	top = data.get("meta", {}).get("top", ["auto"])[0]
-	left = data.get("meta", {}).get("left", ["auto"])[0]
-	right = data.get("meta", {}).get("right", ["auto"])[0]
-	bottom = data.get("meta", {}).get("bottom", ["auto"])[0]
-	width = data.get("meta", {}).get("width", ["auto"])[0]
-	height = data.get("meta", {}).get("height", ["auto"])[0]
-	color = data.get("meta", {}).get("color", ["#fff"])[0]
-	css = "top:" + str(top) + "; left:" + str(left) + "; right:" + str(right) + "; bottom: " + str(bottom) + "; width:" + str(width) + "; height:" + str(height) + "; color: " + str(color)
-	return css
-
-
-def getTemplate(templateName):
-	with open("templates/"+ templateName, "r") as templateFile:
-		return templateFile.read()
-
-
-# --- extract data ------------------------------------------------------------
-data = {}
-dates = []
 settings = {}
-for root, dirs, files in os.walk(inputFolder):
-	for file in files:
-		filePath = os.path.join(root, file)
-		fileParts = os.path.splitext(file)
-		if file == "settings.json":
-			with open("input/settings.json", "r") as settingsFile:
-				settings = json.load(settingsFile)
-			continue
-
-		sorting = [sortingPart.lstrip("0") for sortingPart in fileParts[0].split("_")] 
-		key = "_".join(sorting)
-		ext = fileParts[-1].replace(".", "").lower()
-
-		if key not in data:
-			data[key] = {}
-
-		if ext in imageTypes:
-			filePath = filePath.replace("\\", "/")
-			data[key]["image"] = filePath
-		if ext in textTypes:
-			with open(filePath, "r") as textFile:
-				textContent = textFile.read()
-				htmlContent = md.reset().convert(textContent)
-				meta = md.Meta								
-				data[key]["text"] = htmlContent				
-				data[key]["meta"] = meta
-
-				if "date" in meta:
-					dateObject = strptime(meta.get("date")[0], "%Y-%m-%d")					
-					dateItem = {
-						"date": dateObject,
-						"title": meta.get("title", "")
-					}
-					dates.append(dateItem)
 
 
-# --- sort & arrange ----------------------------------------------------------
-output = OrderedDict()
-for d in data:
-	sorting = d.split("_")
-	i0 = int(sorting[0])
-	i1 = int(sorting[1]) if len(sorting) > 1 else -1
+# --- Directory parsing -------------------------------------------------------
+def parseDirectory(directory):
+	global settings
 
-	if i1 > 0:
-		if i0 not in output:
-			output[i0] = {"children": []}
-		output[i0]["children"].append(data[d])
-	else:
-		output[i0] = data[d]
+	filePattern = re.compile("(\d+)(_?)(\d*).(\w+)")
+	items = {}
 
-output = OrderedDict(sorted(output.items(), key=lambda t: t))
-#todo: sort items with children
+	for name in os.listdir(directory):			
+		match = filePattern.match(name)
+		if match:
+			key = match.group(1)
+			subkey = match.group(3)
+			type = match.group(4).lower()
+
+			if key not in items:
+				items[key] = {}
+
+			if subkey and "children" not in items[key]:
+				items[key]["children"] = {}
+
+			if subkey and subkey not in items[key]["children"]:
+				items[key]["children"][subkey] = {}
+
+			if type in imageTypes:
+				if subkey:
+					items[key]["children"][subkey]["image"] = match.group(0)
+				else:
+					items[key]["image"] = match.group(0)
+
+			if type in videoTypes:
+				if subkey:
+					items[key]["children"][subkey]["video"] = match.group(0)
+				else:
+					items[key]["video"] = match.group(0)
+
+			if type in textTypes:
+				with open(os.path.join(directory, name), "r") as textFile:
+					content = markdownParser.reset().convert(textFile.read())
+					meta = markdownParser.Meta
+
+					if subkey:
+						items[key]["children"][subkey]["text"] = content
+						if meta:
+							items[key]["children"][subkey]["meta"] = meta
+					else:
+						items[key]["text"] = content
+						if meta:
+							items[key]["meta"] = meta
+
+	return items
 
 
-# --- generate html -----------------------------------------------------------
-htmlTarget = "output/index.html"
-if not os.path.exists("output"):
-	os.makedirs("output")
+def listDirectories():
+	directories = {}
+	directoryPattern = re.compile("(\d+) - (.+)")
+	for root, dirs, files in os.walk("."):
+		for dir in dirs:
+			match = directoryPattern.match(dir)
+			if match:
+				items = parseDirectory(os.path.join(root, dir))				
+				key = match.group(1)
+				name = match.group(2)								
+				directory = {
+					"dir": dir,
+					"root": root,
+					"name": name,
+					"items": items,
+					"count": len(items)
+				}
 
-templateBase = getTemplate("template.html")
-templateCss = getTemplate("style.css")
-templateItemSingle = getTemplate("item-single.html")
-templateItemMultiple = getTemplate("item-multiple.html")
-templateContentText = getTemplate("content-text.html")
+				# set settings if available
+				if os.path.isfile(os.path.join(root, dir, "settings.json")):
+					with open(os.path.join(root, dir, "settings.json"), "r") as settingsFile:
+						directory["settings"] = json.load(settingsFile)
 
-template = templateBase.replace("{{css}}", templateCss)		
+				rootFolder = root.split("\\")[-1:][0]
 
-if not os.path.exists(os.path.dirname(htmlTarget)):
-	os.makedirs(os.path.dirname(htmlTarget))
+				if rootFolder is ".":
+					directories[key] = directory					
+					directories[key]["children"] = {}
+					directories[key]["childcount"] = 0
+				else:
+					rootMatch = directoryPattern.match(rootFolder)
+					rootKey = rootMatch.group(1)
+					rootName = rootMatch.group(2)
 
-with open(htmlTarget, "w") as htmlFile:
-	html = ""
+					directories[rootKey]["children"][key] = directory					
+					directories[rootKey]["childcount"] += 1
 
-	dateList = ""
-	for date in dates:
-		d = strftime("%Y-%m-%d", date.get("date"))
-		label = strftime("%d.%m.%Y", date.get("date"))
-		title = date.get("title", "")[0]
-		if title is not "":
-			label += " - " + title
-		dateList += "<a href='#" + d + "'>" + label + "</a>"
-	template = template.replace("{{dates}}", dateList)
+	return directories
 
-	for o in output:
-		if "children" in output[o]:
-			childCount = str(len(output[o]["children"]))
-			content = ""
-			for child in output[o]["children"]:
-				content += getSnippet(child)
-			contentTemplate = getTemplate("item-multiple.html")
-			contentTemplate = contentTemplate.replace("{{count}}", childCount)
-			contentTemplate = contentTemplate.replace("{{content}}", content)
-			html += contentTemplate
-		else:
-			content = getSnippet(output[o])
-			contentTemplate = getTemplate("item-single.html")
-			contentTemplate = contentTemplate.replace("{{content}}", content)
-			html += contentTemplate
 
-	template = template.replace("{{title}}", settings.get("title", "untitled"))		
-	template = template.replace("{{expose}}", html)		
-	htmlFile.write(template)
+def orderDictionary(content):
+	content = OrderedDict(sorted(content.items(), key=lambda t: t))		
+	for c in content:
+		if "items" in content[c]:
+			content[c]["items"] = orderDictionary(content[c]["items"])
+		if "children" in content[c]:
+			content[c]["children"] = orderDictionary(content[c]["children"])
+	return content
+
+
+def folderize(content):
+	if not os.path.exists("output"):
+		os.makedirs("output")
+
+	for c in content:
+		createImages("output", content[c])
+		createHtml("output", content[c])
+
+
+def createImages(root, content):
+	folderName = content["name"].lower().replace(" ", "_")
+	target = os.path.join(root, folderName)
+	origin = os.path.join(content["root"], content["dir"])
+
+	# create folder
+	if not os.path.exists(target):
+		os.makedirs(target)
+
+	# copy images
+	if "items" in content:
+		for item in content["items"]:
+			if "image" in content["items"][item]:
+				imageName = content["items"][item]["image"]
+				imageOrigin = os.path.join(origin, imageName)
+				imageTarget = os.path.join(target, imageName)
+				createImage(imageOrigin, imageTarget, 1000)
+
+			if "video" in content["items"][item]:
+				videoName = content["items"][item]["video"]
+				videoOrigin = os.path.join(origin, videoName)
+				videoTarget = os.path.join(target, videoName)
+				createVideo(videoOrigin, videoTarget)
+
+
+			if "children" in content["items"][item]:
+				for child in content["items"][item]["children"]:
+					if "image" in content["items"][item]["children"][child]:
+						imageName = content["items"][item]["children"][child]["image"]
+						imageOrigin = os.path.join(origin, imageName)
+						imageTarget = os.path.join(target, imageName)
+						createImage(imageOrigin, imageTarget, 1000)
+
+					if "video" in content["items"][item]["children"][child]:
+						videoName = content["items"][item]["children"][child]["video"]
+						videoOrigin = os.path.join(origin, videoName)
+						videoTarget = os.path.join(target, videoName)
+						createVideo(videoOrigin, videoTarget)
+
+	if "children" in content:
+		for child in content["children"]:
+			folderName = content["children"][child]["name"].lower().replace(" ", "_")
+			titleOrigin = os.path.join(content["children"][child]["root"], content["children"][child]["dir"], "title.jpg")		
+			titleTarget = os.path.join(target, folderName, "title.jpg")
+			createImage(titleOrigin, titleTarget, 1000)
+
+			createImages(target, content["children"][child])
+
+
+def createImage(origin, target, width):
+	targetPath = target.split(os.path.sep)
+	targetPath.pop()
+	targetPath = os.path.sep.join(targetPath)
+	if not os.path.exists(targetPath):
+		os.makedirs(targetPath)
+
+	img = Image.open(origin)
+	height = int(width * (img.size[1] / img.size[0]))
+	#img = img.resize((width, height), Image.ANTIALIAS)
+	img = img.resize((width, height))
+	img.save(target)
+
+
+def createVideo(origin, target):
+	shutil.copy(origin, target)
+
+
+def createHtml(root, content):
+	folderName = content["name"].lower().replace(" ", "_")
+	target = os.path.join(root, folderName)
+	htmlTarget = os.path.join(target, "index.html")
+
+	createHtmlPage(htmlTarget, content)
+
+	if "children" in content:
+		for child in content["children"]:
+			createHtml(target, content["children"][child])
+
+
+def createHtmlPage(target, content):
+	templateLoader = jinja2.FileSystemLoader(searchpath="templates")
+	templateEnv = jinja2.Environment(loader=templateLoader)
+	template = templateEnv.get_template("photoset.html")
+
+	items = []
+	if "items" in content:
+		for item in content["items"]:
+			items.append(content["items"][item])
+
+	children = []
+	if "children" in content:
+		for child in content["children"]:
+			children.append(content["children"][child])
+
+	templateVars = {
+		"name": content["name"],
+		"items": items,
+		"children": children
+	}
+	outputText = template.render(templateVars)
+	with open(target, "w") as html:
+		html.write(outputText)
+
+
+def createOverview(content):
+	templateLoader = jinja2.FileSystemLoader(searchpath="templates")
+	templateEnv = jinja2.Environment(loader=templateLoader)
+	template = templateEnv.get_template("overview.html")
+
+	sets = []
+	for c in content:
+		sets.append(content[c])
+		titleOrigin = os.path.join(content[c]["dir"], "title.jpg")
+		titleTarget = os.path.join("output", content[c]["name"].lower().replace(" ", "_"), "title.jpg")
+		createImage(titleOrigin, titleTarget, 1000)
+
+
+	templateVars = {
+		"title" : "Photosets",
+		"sets": sets
+	}
+
+	outputText = template.render(templateVars)
+	with open("output/index.html", "w") as html:
+		html.write(outputText)
+
+	# copy css
+	with open("output/style.css", "w") as cssTarget:
+		with open("templates/style.css", "r") as cssOrigin:
+			cssTarget.write(cssOrigin.read())
+
+
+# --- Startup -----------------------------------------------------------------
+content = listDirectories()
+content = orderDictionary(content)
+
+folderize(content)
+createOverview(content)
